420b23810ab6289753d15297b916c0a8b5c56e67
AbhishiktSinha <133574794+AbhishiktSinha@users.noreply.github.com>
2023-11-20 07:54:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,10 +2,11 @@ const sampleObject = { red: "#FF0000", green: "#00FF00", white: "#FFFFFF" };
 
 function hasKey(key) {
   //   write your code here
-	return if(Object.keys(sampleObject).includes(key));
+	if(Object.keys(sampleObject).includes(key))
+		return true;
+	else return false;
 }
 
 // Do not change the code below
 const key = prompt("Enter Key.");
-
 alert(hasKey(key));
